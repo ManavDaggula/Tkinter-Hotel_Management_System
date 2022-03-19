2a5221f882fce6f484ace542a6b397e79fca5cef
ManavDaggula <manavdaggula@gmail.com>
2022-03-19 21:46:48 +0530

    added label to room frames

--- Manav_func.py
+++ Manav_func.py
@@ -4,14 +4,41 @@ tried by Manav.
 '''
 
 import tkinter as tk
-from SQL_queries import *
+import SQL_queries as queries
 import sqlite3 as sql3
 
-def create_room_table(container):
+def create_room_table(container,selected_option):
     # Insert sql logic here
+    # query=""
+    # if (selected_option=="All"):
+    #     query=queries.room_all
+    # elif (selected_option=="Occupied"):
+    #     query=queries.room_occupied
+    # else:
+    #     query=queries.room_empty
+    # conn=sql3.connect("hotel_management.db")
+    # cur=conn.cursor()
+    # cur.execute(query)
+    # result=cur.fetchall()
 
-    row_data=[('1','2','3'),('a','b','c'),('x','y','z')]
-    for x in range(len(row_data)):
-        for y in range(len(row_data[x])):
-            lbl=tk.Label(container,text=row_data[x][y])
-            lbl.grid(row=x,column=y)
+    # cur.close()
+    # conn.close()
+    lbl_header1 = tk.Label(container, text="Room No")
+    lbl_header1.grid(row=0,column=0)
+    lbl_header2 = tk.Label(container, text="Availability")
+    lbl_header2.grid(row=0,column=1)
+    lbl_header3 = tk.Label(container, text="Room Type")
+    lbl_header3.grid(row=0,column=2)
+    lbl_header4 = tk.Label(container, text="Customer ID")
+    lbl_header4.grid(row=0,column=3)
+
+def list_customers():
+    query=queries.list_customers
+    conn=sql3.connect("hotel_management.db")
+    cur=conn.cursor()
+    cur.execute(query)
+    result=cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return list(result)
--- Room_Status.py
+++ Room_Status.py
@@ -8,24 +8,27 @@ root=tk.Tk() # temporary root window
 frm_room_status = tk.Frame(root,bg="#0d3c59") # do not forget to place the correct master window
 frm_room_status.pack(fill=tk.BOTH)
 
-# Label to indicate Room Status
+# Label to indicate we are on Room Status page
 lbl_room_status=tk.Label(frm_room_status, text="Room Status", font=("Segoe Script",20), bg="#0d3c59", fg="#0099ff")
 lbl_room_status.pack(fill=tk.X)
 
 # Checkboxes to select display category
 selected_option = tk.StringVar() #
 selected_option.set("All")
-chb_occupied=tk.Radiobutton(frm_room_status, text="Occupied", variable=selected_option, value="Occupied", bg="#0d3c59", fg="#0099ff", font=(14))
-chb_empty=tk.Radiobutton(frm_room_status, text="Empty", variable=selected_option, value="Empty", bg="#0d3c59", fg="#0099ff", font=(14))
-chb_all=tk.Radiobutton(frm_room_status, text="All", variable=selected_option, value="All", bg="#0d3c59", fg="#0099ff", font=(14))
-chb_occupied.pack(side=tk.LEFT,fill=tk.X)
+# Frame to hold selection options
+frm_select_room = tk.Frame(frm_room_status, bg="#0d3c59")
+frm_select_room.pack(fill=tk.X)
+chb_occupied=tk.Radiobutton(frm_select_room, text="Occupied", variable=selected_option, value="Occupied", bg="#0d3c59", fg="#0099ff", font=(14))
+chb_empty=tk.Radiobutton(frm_select_room, text="Empty", variable=selected_option, value="Empty", bg="#0d3c59", fg="#0099ff", font=(14))
+chb_all=tk.Radiobutton(frm_select_room, text="All", variable=selected_option, value="All", bg="#0d3c59", fg="#0099ff", font=(14))
+chb_occupied.pack(side=tk.LEFT)
 chb_empty.pack(side=tk.LEFT)
-chb_all.pack(side=tk.LEFT,fill=tk.X)
+chb_all.pack(side=tk.LEFT)
 
 # Creating a Frame to display the Table
-frm_table = tk.Frame(root, bg="#0d3c59")
+frm_table = tk.Frame(frm_room_status, width=100, height=100, bg="#0d3c59")
 # Now creating a table i.e. a bunch of labels
-func.create_room_table(frm_table)
-frm_table.pack()
+func.create_room_table(frm_table,selected_option)
+frm_table.pack(fill=tk.X)
 
 root.mainloop()
