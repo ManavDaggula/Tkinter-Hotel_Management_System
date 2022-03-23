589807c30dfdb9eb4a09da554d7dff7df2cbb689
ManavDaggula <manavdaggula@gmail.com>
2022-03-23 06:22:26 +0530

    room display without scrolling

--- Manav_func.py
+++ Manav_func.py
@@ -8,43 +8,56 @@ import SQL_queries as queries
 import sqlite3 as sql3
 
 def create_room_table(container,selected_option):
+    # CLEARING THE FRAME
+    for w in container.winfo_children():
+        w.destroy()
+
     # Insert sql logic here
+    #print("called")
     query=""
-    if (selected_option=="All"):
+    if (selected_option.get()=="All"):
         query=queries.room_all
-    elif (selected_option=="Occupied"):
+    elif (selected_option.get()=="Occupied"):
         query=queries.room_occupied
     else:
         query=queries.room_empty
     conn=sql3.connect("hotel_management.db")
     cur=conn.cursor()
     cur.execute(query)
+    print(query)
     result=cur.fetchall()
+    print(selected_option)
+    print(result)
     cur.close()
     conn.close()
     # ADDING HEADERS FOR COLUMNS
-    lbl_header1 = tk.Label(container, text="Room No")
+    lbl_header1 = tk.Label(container, text="Room No",bg="#0d3c59",fg="#ffffff")
     lbl_header1.grid(row=0,column=0)
-    lbl_header2 = tk.Label(container, text="Availability")
+    lbl_header2 = tk.Label(container, text="Availability",bg="#0d3c59",fg="#ffffff")
     lbl_header2.grid(row=0,column=1)
-    lbl_header3 = tk.Label(container, text="Room Type")
+    lbl_header3 = tk.Label(container, text="Room Type",bg="#0d3c59",fg="#ffffff")
     lbl_header3.grid(row=0,column=2)
-    lbl_header4 = tk.Label(container, text="Customer ID")
+    lbl_header4 = tk.Label(container, text="Customer ID",bg="#0d3c59",fg="#ffffff")
     lbl_header4.grid(row=0,column=3)
     # ADDING THE ROWS
     row_count=1
     for row in result:
-        lbl1 = tk.Label(container,text=row[0])
+        # print(row)
+        lbl1 = tk.Label(container,text=row[0],bg="#0d3c59",fg="#ffffff")
         lbl1.grid(row=row_count,column=0)
-        lbl2 = tk.Label(container,text="Free")
-        if(row[2]==None):
+        lbl2 = tk.Label(container,text="Free",bg="#0d3c59",fg="#00ff00")
+        if(row[2]!=None):
             lbl2['text']="Occupied"
+            lbl2['fg']='#ffffff'
         lbl2.grid(row=row_count,column=1)
-        lbl3 = tk.Label(container,text=row[1])
+        lbl3 = tk.Label(container,text=row[1],bg="#0d3c59",fg="#ffffff")
         lbl3.grid(row=row_count,column=2)
-        lbl4 = tk.Label(container, text=row[2])
+        lbl4 = tk.Label(container, text=row[2],bg="#0d3c59",fg="#ffffff")
         lbl4.grid(row=row_count,column=3)
         row_count+=1
+    # column expansion set to true
+    for i in range(4):
+        container.columnconfigure(i,weight=1)
 
 
 def list_customers():
@@ -57,3 +70,8 @@ def list_customers():
     cur.close()
     conn.close()
     return list(result)
+
+def temp(container):
+    for w in container.winfo_children():
+        #w.grid_forget()
+        w.destroy()
--- Room_Status.py
+++ Room_Status.py
@@ -18,21 +18,25 @@ selected_option.set("All")
 # Frame to hold selection options
 frm_select_room = tk.Frame(frm_room_status, bg="#0d3c59")
 frm_select_room.pack(fill=tk.X)
-chb_occupied=tk.Radiobutton(frm_select_room, text="Occupied", variable=selected_option, value="Occupied", bg="#0d3c59", fg="#0099ff", font=(14))
-chb_empty=tk.Radiobutton(frm_select_room, text="Empty", variable=selected_option, value="Empty", bg="#0d3c59", fg="#0099ff", font=(14))
-chb_all=tk.Radiobutton(frm_select_room, text="All", variable=selected_option, value="All", bg="#0d3c59", fg="#0099ff", font=(14))
+chb_occupied=tk.Radiobutton(frm_select_room, text="Occupied", variable=selected_option, value="Occupied", bg="#0d3c59", fg="#0099ff", font=(14),command=lambda : func.create_room_table(frm_table,selected_option))
+chb_empty=tk.Radiobutton(frm_select_room, text="Empty", variable=selected_option, value="Empty", bg="#0d3c59", fg="#0099ff", font=(14),command=lambda : func.create_room_table(frm_table,selected_option))
+chb_all=tk.Radiobutton(frm_select_room, text="All", variable=selected_option, value="All", bg="#0d3c59", fg="#0099ff", font=(14),command=lambda : func.create_room_table(frm_table,selected_option))
 chb_occupied.pack(side=tk.LEFT)
 chb_empty.pack(side=tk.LEFT)
 chb_all.pack(side=tk.LEFT)
 
-# Creating a Frame to display the Table
-frm_table = tk.Frame(frm_room_status, bg="#0d3c59")
-frm_table.pack()
+# Creating a Frame to display the Table within a canvas within outer frame
+outerframe = tk.Frame(root,bg='green')
+outerframe.pack(fill=tk.X)
+outerframe.columnconfigure(0,weight=1)
+canvas = tk.Canvas(outerframe)
+frm_table = tk.Frame(outerframe, bg="#0d3c59")
+sb=tk.Scrollbar(outerframe)
+sb.pack(side=tk.RIGHT)
+canvas.pack(side=tk.LEFT,expand=True)
+canvas.create_window(0,0,anchor=tk.NW,window=frm_table)
+# frm_table.pack(fill=tk.X)
 # Now creating a table i.e. a bunch of labels
 func.create_room_table(frm_table,selected_option)
-# Adding scrollbar
-vscroll = tk.Scrollbar(frm_room_status,orient=tk.VERTICAL,command=frm_table.yview)
-frm_table.config(yscrollcommand=vscroll.set)
-vscroll.pack(side=tk.RIGHT, fill=tk.Y)
 
 root.mainloop()
